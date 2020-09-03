7026033705b37c25ab252ab26af3643da3928a89
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-09-04 00:29:21 +0800

    chore: add test case for load_dataset function

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -11,7 +11,12 @@ def test_create_dataloader():
 
 def test_load_dataset():
     (train_vectors, train_labels, vectorizer) = load_dataset("data/ag_news.train")
+    (test_vectors, test_labels, _) = load_dataset("data/ag_news.test")
     assert len(train_vectors.shape) == 2
     assert train_vectors.shape == (119843, 2000)
     assert len(train_labels.shape) == 1
     assert train_labels.shape == (119843,)
+    assert len(test_vectors.shape) == 2
+    assert test_vectors.shape == (7600, 2000)
+    assert len(test_labels.shape) == 1
+    assert test_labels.shape == (7600,)
