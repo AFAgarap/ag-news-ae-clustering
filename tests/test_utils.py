aaa61cda1741f416598bcde6e3d97ee7cf9af29e
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-09-04 00:39:35 +0800

    chore: add test for create_dataloader function

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import torch
+
 from ae_clustering.utils import clustering_accuracy, create_dataloader, load_dataset
 
 
@@ -11,7 +13,27 @@ def test_clustering_accuracy():
 
 
 def test_create_dataloader():
-    pass
+    batch_size = 64
+    (train_vectors, train_labels, _) = load_dataset("data/ag_news.train")
+    (test_vectors, test_labels, _) = load_dataset("data/ag_news.test")
+    train_loader = create_dataloader(
+        features=train_vectors, labels=train_labels, batch_size=batch_size
+    )
+    test_loader = create_dataloader(
+        features=test_vectors, labels=test_labels, batch_size=batch_size
+    )
+    assert isinstance(train_loader, torch.utils.data.DataLoader)
+    assert isinstance(test_loader, torch.utils.data.DataLoader)
+    for batch_features, batch_labels in train_loader:
+        assert isinstance(batch_features, torch.Tensor)
+        assert isinstance(batch_labels, torch.Tensor)
+        assert batch_features.size() == (batch_size, 2000)
+        break
+    for batch_features, batch_labels in test_loader:
+        assert isinstance(batch_features, torch.Tensor)
+        assert isinstance(batch_labels, torch.Tensor)
+        assert batch_features.size() == (batch_size, 2000)
+        break
 
 
 def test_load_dataset():
