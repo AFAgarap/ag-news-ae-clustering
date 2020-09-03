f124ab181b9ae9342a8b7a5dd125aee0b9b6da6c
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-09-03 22:17:49 +0800

    docs: add module header

--- ae_clustering/utils.py
+++ ae_clustering/utils.py
@@ -1,9 +1,27 @@
+# AG News Clustering with Autoencoder
+# Copyright (C) 2020  Abien Fred Agarap
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""Utility functions module"""
 from typing import Dict, Tuple
 
 import nltk
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 import torch
+
+__author__ = "Abien Fred Agarap"
 
 
 def read_data(corpus_file: str, label_column: int, document_start: int) -> Dict:
