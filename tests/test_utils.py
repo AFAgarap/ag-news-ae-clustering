6b0e3c2ee73de2860711dcad12bdf2d70435cd7c
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-09-04 00:33:44 +0800

    chore: add test case for clustering_accuracy function

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,13 @@
+import numpy as np
 from ae_clustering.utils import clustering_accuracy, create_dataloader, load_dataset
 
 
 def test_clustering_accuracy():
-    pass
+    vector_a = np.array([1, 2, 3, 4, 5, 6, 7])
+    vector_b = np.array([1, 2, 1, 4, 1, 6, 7])
+    expected_output = 0.7142857142857143
+    output = clustering_accuracy(vector_a, vector_b)
+    assert np.allclose(output, expected_output)
 
 
 def test_create_dataloader():
